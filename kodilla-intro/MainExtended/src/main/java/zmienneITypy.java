cb99ad4dbe1e0c7e7a692f12b38e2ecd6d320e03
Jadwiga Jedrzej <jadwigajedrzejewska2024@gmail.com>
2024-06-04 13:21:07 +0200

    modul 0.8

---
+++ kodilla-intro/MainExtended/src/main/java/zmienneITypy.java
@@ -0,0 +1,17 @@
+public class zmienneITypy {
+    public static void main(String[] args) {
+        String zdanie = "Ciąg znaków różnych 123!";
+        int liczbaCalkowita = 123;
+        double liczbaDziesietna = 1.234;
+        char symbol = '$';
+        boolean takCzyNie = true;
+        boolean nieCzyTak = false;
+        System.out.println(zdanie);
+        System.out.println(liczbaCalkowita);
+        System.out.println(liczbaDziesietna);
+        System.out.println(symbol);
+        System.out.println(takCzyNie);
+        System.out.println(nieCzyTak);
+    }
+}
+
