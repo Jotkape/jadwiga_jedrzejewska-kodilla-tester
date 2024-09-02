68be121fe3e8e768e873bee4ceba1f14d5e9d95d
Jadwiga Jedrzej <jadwigajedrzejewska2024@gmail.com>
2024-09-02 19:04:35 +0200

    moduł 13,5

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,15 +1,15 @@
 package com.kodilla.selenium.allegro;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -21,12 +21,29 @@ public class AllegroTestingApp {
 
         //poniższy kod nie zadziała ponieważ uruchami się antybot mechanizm allegro którego nie da się wyłaczyć wg wskazówek z zadania
 
-        WebElement categoryDropdown = driver.findElement(By.xpath("//select[contains(@aria-label, 'Wybierz kategorię')]"));
+//        WebElement categoryDropdown = driver.findElement(By.xpath("//select[contains(@aria-label, 'Wybierz kategorię')]"));
+//        Select selectCategory = new Select(categoryDropdown);
+//        selectCategory.selectByVisibleText("Elektronika");
+//        WebElement searchBox = driver.findElement(By.xpath("//input[contains(@placeholder, 'czego szukasz')]"));
+//        searchBox.sendKeys("Mavic mini");
+//        searchBox.submit();
+
+        WebElement categoryDropdown = driver.findElement(By.cssSelector("select[aria-label*='Wybierz kategorię']"));
         Select selectCategory = new Select(categoryDropdown);
         selectCategory.selectByVisibleText("Elektronika");
-        WebElement searchBox = driver.findElement(By.xpath("//input[contains(@placeholder, 'czego szukasz')]"));
+
+        WebElement searchBox = driver.findElement(By.cssSelector("input[placeholder*='czego szukasz']"));
         searchBox.sendKeys("Mavic mini");
         searchBox.submit();
-    }
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section > article")));
+
+        List<WebElement> productCards = driver.findElements(By.cssSelector("section > article"));
+
+        for (WebElement product : productCards) {
+            System.out.println(product.getText());
+        }
+
+    }
 }
